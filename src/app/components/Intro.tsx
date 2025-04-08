c136b2c4e5d9c9f5ef281c7d31d097146b044e11
miracalakus <mirac-013@live.nl>
2025-04-08 15:50:03 +0200

    small jsx/tsx fix to build

--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -5,19 +5,19 @@ export default function Introduction() {
   return (
     <section className="h flex flex-col items-center justify-center text-center">
       <div className="text-center mt-12">
-  <h1 className="font-title">Hi, I'm Mirac Alakus</h1>
-  <div className="text-black font-semibold mt-1 flex justify-center gap-2">
-    <h3 className="px-6 py-3 bg-amber-200 rounded-4xl border-2 border-gray-800 ">
-      Front-End Developer
-    </h3>
-    <div className="px-6 py-3 bg-blue-200 rounded-4xl border-2 border-gray-800 ">
-      UI & UX Designer
-    </div>
-    <div className="px-6 py-3 bg-red-200 rounded-4xl border-2 border-gray-800 ">
-      Motion Designer
-    </div>
-  </div>
-</div>
+        <h1 className="font-title">Hi, I&apos;m Mirac Alakus</h1> {/* Escape apostrophe */}
+        <div className="text-black font-semibold mt-1 flex justify-center gap-2">
+          <h3 className="px-6 py-3 bg-amber-200 rounded-4xl border-2 border-gray-800 ">
+            Front-End Developer
+          </h3>
+          <div className="px-6 py-3 bg-blue-200 rounded-4xl border-2 border-gray-800 ">
+            UI &amp; UX Designer {/* Escape the & */}
+          </div>
+          <div className="px-6 py-3 bg-red-200 rounded-4xl border-2 border-gray-800 ">
+            Motion Designer
+          </div>
+        </div>
+      </div>
       <div className='mt-5'>
         <TestComp/>
       </div>
