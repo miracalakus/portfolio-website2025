5d409f43abb589c0bf7e533cb5b313f53c9d4c16
miracalakus <mirac-013@live.nl>
2025-04-11 16:48:10 +0200

    new font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,39 @@
 import "./styles/globals.css";
-import { Montserrat, Poppins, Work_Sans } from 'next/font/google';
+import { Montserrat, DM_Serif_Display, Poppins, Work_Sans } from 'next/font/google';
 
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 
+export const metadata = {
+  title: "Mirac's Portfolio",
+  description: "Creative front-end developer and UX/UI designer with 3D and motion design skills. Explore my work and projects.",
+};
+
 // Load fonts with specific weights and subsets
-const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '700','900'], variable: '--font-montserrat' });
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'], variable: '--font-poppins' });
-const workSans = Work_Sans({ subsets: ['latin'], weight: ['300', '400', '500'], variable: '--font-worksans' });
+const dmserifdisplay = DM_Serif_Display({
+  weight: '400', // required
+  subsets: ['latin'], // required
+  variable: '--font-dmserifdisplay', // optional, but nice for CSS vars
+});
+const montserrat = Montserrat({ 
+  subsets: ['latin'], 
+  weight: ['400', '700','900'], 
+  variable: '--font-montserrat' 
+});
+const poppins = Poppins({ 
+  subsets: ['latin'], 
+  weight: ['400', '600', '700'], 
+  variable: '--font-poppins' 
+});
+const workSans = Work_Sans({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500'], 
+  variable: '--font-worksans' 
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${poppins.variable} ${workSans.variable}`}>
+    <html lang="en" className={`${dmserifdisplay.variable} ${montserrat.variable} ${poppins.variable} ${workSans.variable}`}>
       <body className="flex flex-col min-h-screen">
         <Navbar/>
         <main className="pt-20">{children}</main>
